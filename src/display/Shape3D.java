d1815de6a61396a7d70c85b2810938aab6ad0f57
alie6645 <70154195+alie6645@users.noreply.github.com>
2023-01-31 09:18:38 -0700

    Delete src directory

--- src/display/Shape3D.java
+++
@@ -1,9 +0,0 @@
-package display;
-
-import java.awt.*;
-import java.util.List;
-
-public interface Shape3D {
-    public List<Line3D> getLines();
-    public Color getColor();
-}
--- src/src/display/shape/Shape3D.java
+++
@@ -1,9 +0,0 @@
-package display.shape;
-
-import java.awt.*;
-import java.util.List;
-
-public interface Shape3D {
-    public List<Line3D> getLines();
-    public Color getColor();
-}
